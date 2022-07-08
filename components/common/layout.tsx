f0d1683b9c519dc7e2152581fe2a39e3583f5a39
Abigail Folarin <folarinabigail2020@gmail.com>
2022-07-08 17:13:23 +0100

    add-layout

---
+++ components/common/layout.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+type Props = {
+  children: any;
+};
+
+const Layout: React.FC<Props> = ({ children }) => (
+  <div className="flex h-screen">
+    <div className="flex-none bg-violet w-20 h-full" />
+    <div className="w-128 bg-white h-full container mx-auto">
+      <p className="mt-28 ml-28 mb-7 text-violet font-bold text-5xl">
+        Nut
+        <span className="font-normal">Available</span>
+      </p>
+      <div>{children}</div>
+    </div>
+  </div>
+);
+
+export default Layout;
